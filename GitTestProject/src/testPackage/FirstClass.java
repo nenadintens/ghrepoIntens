394f1d21c82bba9eeb18744908306138e0cd5961
nenad.nikolic <nenad.nikolic@vastera17.intens.local>
2018-03-13 11:47:44 +0100

    First Commit

---
+++ GitTestProject/src/testPackage/FirstClass.java
@@ -0,0 +1,11 @@
+package testPackage;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is not X-Men first class");
+
+	}
+
+}
