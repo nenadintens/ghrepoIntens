cbc2f70b86be6d0ddad674ac6d04269ef687b7c9
Daniel Fath <daniel.fath7@gmail.com>
2018-03-13 12:08:58 +0100

    More Commits

--- GitTestProject/src/testPackage/FirstClass.java
+++ GitTestProject/src/testPackage/FirstClass.java
@@ -3,9 +3,8 @@ package testPackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("This is not X-Men first class");
+		System.out.println("This is not Z-Men first class");
 		System.out.println("Rather a test class");
-
 	}
 
 }
