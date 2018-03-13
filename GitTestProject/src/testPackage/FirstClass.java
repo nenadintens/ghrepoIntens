2ae4d7a92e0de04de3381254f217fc8fa37df953
nenad.nikolic <nenad.nikolic@vastera17.intens.local>
2018-03-13 11:50:07 +0100

    Second class changes

--- GitTestProject/src/testPackage/FirstClass.java
+++ GitTestProject/src/testPackage/FirstClass.java
@@ -5,6 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is not X-Men first class");
+		
+		System.out.println("Rather a test class");
 
 	}
 
